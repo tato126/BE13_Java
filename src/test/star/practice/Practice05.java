5912f1a8f7a70c089f5019b5cfe5449ee369565b
Tato <heechan.log@gmail.com>
2025-06-18 16:11:05 +0900

    혼자서 별 찍기 5번 문제 (해결)

---
+++ src/test/star/practice/Practice05.java
@@ -0,0 +1,13 @@
+package test.star.practice;
+
+public class Practice05 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
