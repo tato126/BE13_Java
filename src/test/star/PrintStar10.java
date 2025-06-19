91d61000bb8d2f38176993333f8aa960493f9057
Tato <heechan.log@gmail.com>
2025-06-19 13:54:27 +0900

    별 찍기 10번 문제 (해결)

---
+++ src/test/star/PrintStar10.java
@@ -0,0 +1,25 @@
+package test.star;
+
+public class PrintStar10 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(String.format("[%d, %d]", i, j));
+            }
+            System.out.println();
+        }
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+
+                if (i < j && i + j < 6 || i > j && i + j > 6) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
